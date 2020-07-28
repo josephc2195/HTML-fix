a16b9eb5c9da45476ea2f842be721a8ac12e5886
Joseph <chicaj1221@gmail.com>
2020-07-28 11:50:58 -0400

    update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, redirect
 import requests
 import os
 from flask_bootstrap import Bootstrap
-import json
 
 app = Flask(__name__)
 Bootstrap(app)
