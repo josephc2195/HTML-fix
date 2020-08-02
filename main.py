15ac87b69f90825affc7539b8b13e17a8b8ebb81
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2020-08-02 12:31:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,8 @@ import requests
 import os
 from flask_bootstrap import Bootstrap
 import json
-from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.config.update(
-	MAIL_SERVER='smtp.gmail.com',
-	MAIL_PORT=465,
-	MAIL_USE_SSL=True,
-	MAIL_USERNAME = 'chicafixit@gmail.com',
-	MAIL_PASSWORD = 'Maiden2014!'
-)
-
-mail = Mail(app)
-
 Bootstrap(app)
 
 @app.route('/')
@@ -26,11 +15,10 @@ def index():
 @app.route('/get_message', methods=['GET', 'POST'])
 def get_message():
 	if request.method == 'POST':
-		name = request.form['name']
-		email = request.form['email']
-		phone = request.form['phone']
-		message = request.form['message']
-		
+		firstName = request.form['firstName']
+		lastName = request.form['lastName']
+		message = request.form['subject']
+		print(firstName, lastName, message)
 		return redirect(url_for('typ'))
 
 	else:
@@ -41,15 +29,6 @@ def typ():
 	error = request.args.get('error')
 	return render_template('typ.html', error=error)
 
-@app.route('/send-mail/')
-def send_mail():
-	msg = mail.send_message(
-		'tutorial',
-		sender='chicaj1221@gmail.com',
-		recipients=['chicafixit@gmail.com'],
-		body='it works'
-	)
-	return 'mail sent'
 
 port = int(os.environ.get('POST', 5000))
 if __name__ == '__main__':
