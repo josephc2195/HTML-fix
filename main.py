4fdb39051ae0a8501b6f7b07d4b4977e382f9045
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2020-08-01 22:58:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import requests
 import os
 from flask_bootstrap import Bootstrap
 import json
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config.update(
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = 'chicafixit@gmail.com',
+	MAIL_PASSWORD = 'Maiden2014!'
+)
+
+mail = Mail(app)
+
 Bootstrap(app)
 
 @app.route('/')
@@ -30,6 +41,15 @@ def typ():
 	error = request.args.get('error')
 	return render_template('typ.html', error=error)
 
+@app.route('/send-mail/')
+def send_mail():
+	msg = mail.send_message(
+		'tutorial',
+		sender='chicaj1221@gmail.com',
+		recipients=['chicafixit@gmail.com'],
+		body='it works'
+	)
+	return 'mail sent'
 
 port = int(os.environ.get('POST', 5000))
 if __name__ == '__main__':
